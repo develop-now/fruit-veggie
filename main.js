24fb08fecb1e97feacb34f3da05a98e6391323df
jieun <developnow55@gmail.com>
2021-06-22 00:30:51 +0900

    Connect with Window.js to open a main window

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 'use strict'
+const path = require('path')
+const { app } = require('electron')
 
-console.log("git test2")
-console.log("git test 4")
-console.log("git test 6")
+const Window = require('./Window')
+
+function main () {
+    let mainWindow = new Window({
+        file: path.join('renderer', 'index.html'),
+        webPreferences: {
+            NodeIterator: true,
+            contextIsolation: false
+        }
+    })
+
+    mainWindow.once('show')
+}
+
+app.on('ready', main)
+
+app.on('window-all-closed', () => {
+    app.quit()
+})
+
