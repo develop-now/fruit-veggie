f1a83d42de3c786c054648f88fe6e391377832cb
jieun <developnow55@gmail.com>
2021-06-24 22:48:03 +0900

    Add the way to modify submitted data

--- renderer/index.js
+++ renderer/index.js
@@ -6,16 +6,6 @@ require('bootstrap')
 let randomNum = 0;
 let hasNumResult = true;
 
-// checked - value: true, unchecked -value: false
-$(".big-checkbox").on('click', function () {
-    let value = this.value;
-
-    if (this.value === 'false') {
-        this.value = 'true';
-    }else {
-        this.value = 'false';
-    }
-});
 
 // randomNum for product id
 function getRandomNum(min, max) {
@@ -24,17 +14,16 @@ function getRandomNum(min, max) {
     return Math.floor(Math.random() * (max_ - min_ +1)) + min_ ;
 }
 
+// check randomNum has been existed
 function hasNum() {
-
+    
     randomNum = getRandomNum(1,200);
     let numArray = []
-
-    // get all values from class="num"
+    
     $(".num").each(function () {
         numArray.push(this.value);
     })
     
-    // check randomNum has been existed
     let i = 0;
     while (i <= (numArray.length - 1)) {
         if(randomNum == numArray[i]) {
@@ -46,18 +35,73 @@ function hasNum() {
     }
 }
 
-// TODO : when submit the form, change checkbox value to false and unchecked for state
-$("form").on('submit', function (e) {
+// checked - value: true, unchecked - value: false
+$(".big-checkbox").on('click', function () {
 
-    e.preventDefault();
+    if (this.value === 'false') {
+        this.value = 'true';
+    }else {
+        this.value = 'false';
+    }
+});
+
+// put randomNum to input tag (name="submitNum") in form
+$("input[name=name]").on('click', function() {
     hasNum();
 
-    // if hasNumResult = true, call hasNum() until hasNumTesult = false
+    // if hasNumResult = true, callback hasNum() until hasNumResult = false
     if(hasNumResult) {
 
         while(hasNumResult) {
             hasNum();
             
         }
-    }  
+    }
+    $("input[name=submitNum]").val(randomNum);
+
+})
+
+// init defaultVal 
+$(".defaultVal").on('click', function () {
+    if(this.value == 0){
+        this.value = '';
+    }
+})
+$(".defaultVal").on('keydown', function () {
+    if(this.value == 0){
+        this.value = '';
+    }
+})
+
+// change defaultVal color and value 
+$(".defaultVal").on('keyup', function () {
+    if(this.value != '') {
+        this.style.color = 'black';
+    }else if(this.value === '') {
+        this.value = 0;
+        this.style.color = 'rgb(146, 143, 143)';
+    }
+})
+
+// modify the submited data 
+$("form").on('submit', function (e) {
+
+    e.preventDefault();
+    let formData = $(this).serializeArray();
+    let ObjectedData = new Object();
+
+    $(".big-checkbox").prop("checked", false);
+    $(".big-checkbox").val("false");
+    
+    for(const keys in formData) {
+        
+        let key = formData[keys].name;
+        let value = formData[keys].value;
+
+        ObjectedData[key] = value;
+
+    }
+
+    let jsonData = JSON.stringify(ObjectedData);
+    
 })
