f19382a3e8ce7028ae3abb12893ebfaefa00e763
jieun <developnow55@gmail.com>
2021-07-06 01:20:48 +0900

    Fix the return type error

--- ConfirmDataStore.js
+++ ConfirmDataStore.js
@@ -5,23 +5,24 @@ const DataStore = require('./DataStore')
 class ConfirmDataStore extends DataStore {
     
     modifyProduct(confirm) {
-
         let confirmations = this.getProducts()
         let index = confirm.index
         if(confirm.name === confirmations[index].name && confirm.confirmQuantity === 0 && confirm.confirmPrice === 0) {
-            return
-            }
-            
-            confirmations[index].confirmQuantity = confirm.confirmQuantity
-            confirmations[index].confirmPrice = confirm.confirmPrice
-            
-            this.products = confirmations
-
+            return this
+        }
+        
+        confirmations[index].confirmQuantity = confirm.confirmQuantity
+        confirmations[index].confirmPrice = confirm.confirmPrice
+        
+        console.log(' before' + this.products)
+        this.products = confirmations
+        
+        console.log(' after' + this.products)
         if(confirm.name !== confirmations[index].name){
-
+            
             confirmations[index].name = confirm.name
             this.products = confirmations
-
+            
             return this.sortProducts()
         }
         
