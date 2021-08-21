6f673bec1969af22c4de31a0390c54214c5202ce
jieun <developnow55@gmail.com>
2021-08-21 22:29:17 +0900

    Add eslint and prettier and fix

--- ConfirmDataStore.js
+++ ConfirmDataStore.js
@@ -1,31 +1,31 @@
-'use strict'
-
-const DataStore = require('./DataStore')
+const DataStore = require('./DataStore');
 
 class ConfirmDataStore extends DataStore {
-    
-    modify(confirm) {
-        let confirmations = this.getProducts()
-        let index = confirm.index
-        if(confirm.name === confirmations[index].name && confirm.confirmQuantity === 0 && confirm.confirmPrice === 0) {
-            return this
-        }
-        
-        confirmations[index].confirmQuantity = confirm.confirmQuantity
-        confirmations[index].confirmPrice = confirm.confirmPrice
-        
-        this.products = confirmations
-        
-        if(confirm.name !== confirmations[index].name){
-            
-            confirmations[index].name = confirm.name
-            this.products = confirmations
-            
-            return this.sort()
-        }
-        
-        return this.save()
+  modify(confirm) {
+    const confirmations = this.getProducts();
+    const { index } = confirm;
+    if (
+      confirm.name === confirmations[index].name &&
+      confirm.confirmQuantity === 0 &&
+      confirm.confirmPrice === 0
+    ) {
+      return this;
     }
+
+    confirmations[index].confirmQuantity = confirm.confirmQuantity;
+    confirmations[index].confirmPrice = confirm.confirmPrice;
+
+    this.products = confirmations;
+
+    if (confirm.name !== confirmations[index].name) {
+      confirmations[index].name = confirm.name;
+      this.products = confirmations;
+
+      return this.sort();
+    }
+
+    return this.save();
+  }
 }
 
-module.exports = ConfirmDataStore
+module.exports = ConfirmDataStore;
--- renderer/price-table.js
+++ renderer/price-table.js
@@ -1,39 +1,39 @@
-'use strict'
+const { ipcRenderer } = require('electron');
 
-const { ipcRenderer } = require("electron")
+ipcRenderer.on('confirm-Price', (event, data) => {
+  const products = data[0];
+  const confirmations = data[1];
+  const priceTbody = document.getElementById('priceT_tbody');
+  let html = '';
 
-ipcRenderer.on('confirm-Price', function (event, data) {
-    const products = data[0]
-    const confirmations = data[1]
-    const priceTbody = document.getElementById('priceT_tbody')
-    let html = ''
-    
-    confirmations.forEach(function (confirmation, index, confirmations) {
-        let comfirmPrice = confirmation.confirmPrice.toString()
-        
-        if( comfirmPrice.length > 3 ) {
-            const confirmPriceReverse = comfirmPrice.split('').reverse()
-            confirmPriceReverse.splice(3,0,',')
-            const confirmPriceComma = confirmPriceReverse.reverse().join('')
-            comfirmPrice = confirmPriceComma
-        }
+  confirmations.forEach((confirmation, index) => {
+    let comfirmPrice = confirmation.confirmPrice.toString();
 
-        if(index%2 === 0){
-            html +=`<tr class="tr_bg"><td>${index+1}</td>`
-        }else {
-            html +=`<tr><td>${index+1}</td>`
-        }
-        html +=`<td><span>${confirmation.name}</span></td>`
-            +`<td><span>${confirmation.confirmQuantity}</span>&nbsp;<span class="lightText">`
-            if(products[index].box_kg !== 0) {
-                html += `g`
-            }else {
-                html += `개`
-            }
-        html +=`</span></td>`
-            +`<td><span>${comfirmPrice}</span>&nbsp;<span class="lightText">원</span></td>`
-            +`<tr>`
-    })
-    
-    priceTbody.innerHTML = html
-})
+    if (comfirmPrice.length > 3) {
+      const confirmPriceReverse = comfirmPrice.split('').reverse();
+      confirmPriceReverse.splice(3, 0, ',');
+      const confirmPriceComma = confirmPriceReverse.reverse().join('');
+      comfirmPrice = confirmPriceComma;
+    }
+
+    if (index % 2 === 0) {
+      html += `<tr class="tr_bg"><td>${index + 1}</td>`;
+    } else {
+      html += `<tr><td>${index + 1}</td>`;
+    }
+    html +=
+      `<td><span>${confirmation.name}</span></td>` +
+      `<td><span>${confirmation.confirmQuantity}</span>&nbsp;<span class="lightText">`;
+    if (products[index].box_kg !== 0) {
+      html += 'g';
+    } else {
+      html += '개';
+    }
+    html +=
+      '</span></td>' +
+      `<td><span>${comfirmPrice}</span>&nbsp;<span class="lightText">원</span></td>` +
+      '<tr>';
+  });
+
+  priceTbody.innerHTML = html;
+});
