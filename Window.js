6a95cfc00cab298039193d5173a2a3972cadf0ba
jieun <developnow55@gmail.com>
2021-08-24 23:56:56 +0900

    Add eslint, prettier

--- Window.js
+++ Window.js
@@ -1,22 +1,20 @@
-'use strict'
-
-const { BrowserWindow } = require('electron')
+const { BrowserWindow } = require('electron');
 
 const defaultSetting = {
-    width: 1250,
-    height: 700,
-    show: false
-}
+  width: 1250,
+  height: 700,
+  show: false,
+};
 
 class Window extends BrowserWindow {
-    constructor ({ file, ...windowSettings }) {
-        super({ ...defaultSetting, ...windowSettings})
-        this.loadFile(file)
-        //this.webContents.openDevTools() 
-        this.once('ready-to-show', () => {
-            this.show()
-        }) 
-    }
+  constructor({ file, ...windowSettings }) {
+    super({ ...defaultSetting, ...windowSettings });
+    this.loadFile(file);
+    // this.webContents.openDevTools()
+    this.once('ready-to-show', () => {
+      this.show();
+    });
+  }
 }
 
-module.exports = Window
+module.exports = Window;
